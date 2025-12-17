fa8719698fc88d22317f27b870b2afbfbb3d6c00
Readright503 <Readright42@gmail.com>
2025-12-17 12:59:16 -0700

    Updated CRMView.tsx

--- src/components/CRMView.tsx
+++ src/components/CRMView.tsx
@@ -243,20 +243,22 @@ export function CRMView({ appointments, selectedClientId, onAppointmentClick }: 
                     <TableCell className="text-right">
                       <div className="flex justify-end gap-2">
                         <Button
-                          variant="ghost"
-                          size="icon"
+                          variant="outline"
+                          size="sm"
                           onClick={() => openDetailModal(client)}
-                          className="text-blue-600 hover:text-blue-700 hover:bg-blue-50"
+                          className="bg-white text-black border-gray-300 hover:bg-gray-100"
                         >
-                          <Pencil className="w-4 h-4" />
+                          <Pencil className="w-4 h-4 mr-1" />
+                          Edit
                         </Button>
                         <Button
-                          variant="ghost"
-                          size="icon"
+                          variant="outline"
+                          size="sm"
                           onClick={() => handleDeleteClient(client.id)}
-                          className="text-red-600 hover:text-red-700 hover:bg-red-50"
+                          className="bg-white text-black border-gray-300 hover:bg-gray-100"
                         >
-                          <Trash2 className="w-4 h-4" />
+                          <Trash2 className="w-4 h-4 mr-1" />
+                          Delete
                         </Button>
                       </div>
                     </TableCell>
